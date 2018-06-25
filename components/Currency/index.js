68446ea0d6a6989f28502b75f2191e0e859b4893
ra1111 <rahula@geekyants.com>
2018-06-25 12:40:37 +0530

    fixed size issue

--- components/Currency/index.js
+++ components/Currency/index.js
@@ -6,7 +6,8 @@ import {
   StyleSheet,
   Text,
   Dimensions,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 import * as converter from 'number-to-words';
 import {Icon} from 'react-native-elements';
@@ -20,10 +21,10 @@ import TwentyPence from '../../Assets/Images/TwentyPence.png';
 import TwoPenny from '../../Assets/Images/TwoPenny.png';
 import TwoPounds from '../../Assets/Images/TwoPounds.png';
 import TenPound from '../../Assets/Images/TenPounds.png';
-import TwentyPounds from '../../Assets/Images/TwentyPound.png';
-import TwentyPound from '../../Assets/Images/TwentyPounds.png';
-import FivePound from '../../Assets/Images/FivePound.png';
-import FivePounds from '../../Assets/Images/FivePounds.png';
+// import TwentyPounds from '../../Assets/Images/TwentyPound.png';
+// import TwentyPound from '../../Assets/Images/TwentyPounds.png';
+// import FivePound from '../../Assets/Images/FivePound.png';
+// import FivePounds from '../../Assets/Images/FivePounds.png';
 
 import styles from './styles';
 var {width, height} = Dimensions.get('window');
@@ -36,52 +37,69 @@ let arr = [
   TwentyPence,
   TwoPenny,
   TwoPounds,
-  TenPence,
-  TwentyPounds,
-  TwentyPound,
-  FivePound,
-  FivePounds
+  TenPence
+  // TwentyPounds,
+  // TwentyPound,
+  // FivePound,
+  // FivePounds
 ];
+let ans = 0;
 //100 penies=1 pound
-let val = [50, 5, 1, 100, 10, 2000, 2, 200, 10, 2000, 2000, 500, 500];
+let val = [50, 5, 1, 100, 10, 2000, 2, 200, 10];
 
 export default class Currency extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      number1: 0,
+      number2: 0,
+      ans: 0,
+      option1: 0,
+      option2: 0
+    };
+  }
+  wrongOption() {
+    console.log('Wrong Oprtion selected');
   }
   randomGenerator() {
-    number = Math.floor((Math.random() + 1) * 20);
-    ans = converter.toWords(number);
-    option1 = converter.toWords(number - 1);
-    option2 = converter.toWords(number + 1);
+    let max = val.length;
+    number1 = Math.floor(Math.random() * 10 + 29);
+    number2 = Math.floor(Math.random() * 10 + 23);
+    number1 %= max;
+    number2 %= max;
+
+    ans = val[number1] + val[number2];
     random = Math.floor(Math.random() * 3);
+    option1 = ans + 1;
+    option2 = ans - 1;
     if (random === 3) {
       this.setState({
-        number1: number,
-
+        number1: arr[number1],
+        number2: arr[number2],
         ans: ans,
         option1: option1,
         option2: option2
       });
     } else if (random === 2) {
       this.setState({
-        number1: number,
+        number1: arr[number2],
+        number2: arr[number1],
         ans: option1,
         option1: ans,
         option2: option2
       });
     } else if (random === 1) {
       this.setState({
-        number1: number,
-
+        number1: arr[number2],
+        number2: arr[number1],
         ans: option1,
         option1: option2,
         option2: ans
       });
     } else {
       this.setState({
-        number1: number,
-
+        number1: arr[number2],
+        number2: arr[number1],
         ans: ans,
         option1: option2,
         option2: option1
@@ -90,9 +108,88 @@ export default class Currency extends Component {
   }
   render() {
     return (
-      <View>
-        <Image style={{width: 50, height: 50}} source={OnePound} />
-      </View>
+      <ScrollView>
+        <Image />
+        <View style={stylez.container}>
+          <View style={styles.container}>
+            <View style={styles.questionWrapper}>
+              <View style={styles.question}>
+                <Image
+                  style={styles.questionText}
+                  source={this.state.number2}
+                />
+              </View>
+              <Icon
+                name={this.props.name || 'ios-add'}
+                raised
+                type="ionicon"
+                size={28}
+                color={'orange'}
+              />
+              <View style={styles.question}>
+                <Image
+                  style={styles.questionText}
+                  source={this.state.number1}
+                />
+              </View>
+            </View>
+            <View style={styles.optionWrapper}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={e => {
+                  ans == this.state.ans
+                    ? this.randomGenerator(e)
+                    : this.wrongOption(e);
+                }}
+              >
+                <Text style={styles.buttonText}>{this.state.ans || 2}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={e => {
+                  ans == this.state.option1
+                    ? this.randomGenerator(e)
+                    : this.wrongOption(e);
+                }}
+              >
+                <Text style={styles.buttonText}>
+                  {' '}
+                  {this.state.option1 || 1}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={e => {
+                  ans == this.state.option2
+                    ? this.randomGenerator(e)
+                    : this.wrongOption(e);
+                }}
+              >
+                <Text style={styles.buttonText}>
+                  {' '}
+                  {this.state.option2 || 3}{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+const stylez = StyleSheet.create({
+  time: {
+    height: height, //Dimensions.get('window').height,
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    opacity: 0.8,
+    backgroundColor: '#023367',
+    width: width,
+    zIndex: 1
+  },
+  container: {
+    flex: 1
+  }
+});
---
+++ components/Currency/styles.js
@@ -0,0 +1,67 @@
+export default {
+  container: {
+    backgroundColor: '#186da5',
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  questionWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fbf0e5',
+    height: 160,
+    borderRadius: 8,
+    marginTop: 40,
+    alignSelf: 'center',
+    marginHorizontal: 25,
+    width: '100%'
+  },
+  optionWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingBottom: 20
+  },
+  button: {
+    backgroundColor: '#fbf0e5',
+    borderRadius: 3,
+    height: 50,
+    width: '50%',
+    marginTop: 80,
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: {height: 1, width: 1},
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    elevation: 6,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#1062a4',
+    fontWeight: '700',
+    fontSize: 18,
+    marginTop: 15,
+    textAlign: 'center',
+    alignSelf: 'center'
+  },
+  questionText: {
+    alignSelf: 'center',
+    resizeMode: 'contain',
+    flex: 1
+  },
+  question: {
+    alignSelf: 'center',
+    shadowColor: 'rgba(0,0,0, .4)',
+    shadowOffset: {height: 1, width: 1},
+    shadowOpacity: 1,
+    shadowRadius: 1,
+    backgroundColor: 'orange',
+    height: 80,
+    width: 40,
+    borderRadius: 10
+  },
+  timer: {
+    backgroundColor: '#023667',
+    width: '100%',
+    // height:'100%',
+    zIndex: 100
+  }
+};
